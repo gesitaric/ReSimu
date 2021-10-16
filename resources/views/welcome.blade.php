6a96fcd548e5df49c4a8fb4a35ee3f2a9f92b1a8
italo ricardo geske <italoricardogeske@gmail.com>
2021-10-16 10:37:15 +0900

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">不動産投資シミュレータ</div>
+
+                    <div class="card-body">
+                        <div class="d-grid gap-2 col-6 mx-auto">
+                            <button class="btn btn-primary text-white" type="button">シミュレーション開始</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
